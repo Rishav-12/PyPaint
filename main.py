2a3d37aac877a6a9e24bae21223cfda11a56f400
Rishav Mitra <rishavmitra10@gmail.com>
2022-01-25 19:28:20 +0000

    :sparkles: changed indent size from 2 to 4

--- main.py
+++ main.py
@@ -3,31 +3,31 @@ import pygame
 pygame.init()
 
 class GridSquare:
-  def __init__(self, left, top, width, height):
-    self.rect = pygame.Rect(left, top, width, height)
-    self.color = "white"
-  def draw(self, window):
-    pygame.draw.rect(window, self.color, self.rect)
+    def __init__(self, left, top, width, height):
+        self.rect = pygame.Rect(left, top, width, height)
+        self.color = "white"
+    def draw(self, window):
+        pygame.draw.rect(window, self.color, self.rect)
 
 class ColorButton:
-  def __init__(self, left, top, width, height, color):
-    self.rect = pygame.Rect(left, top, width, height)
-    self.color = color
-  def draw(self, window):
-    pygame.draw.rect(window, self.color, self.rect)
+    def __init__(self, left, top, width, height, color):
+        self.rect = pygame.Rect(left, top, width, height)
+        self.color = color
+    def draw(self, window):
+        pygame.draw.rect(window, self.color, self.rect)
 
 def draw_on_grid(is_drawing, current_square):
-  return is_drawing and current_square.rect.collidepoint(pygame.mouse.get_pos())
+    return is_drawing and current_square.rect.collidepoint(pygame.mouse.get_pos())
 
 def pick_color(current_button):
-  return pygame.mouse.get_pressed()[0] and current_button.rect.collidepoint(pygame.mouse.get_pos())
+    return pygame.mouse.get_pressed()[0] and current_button.rect.collidepoint(pygame.mouse.get_pos())
 
 def pick_clear(clear_rect):
-  return pygame.mouse.get_pressed()[0] and clear_rect.collidepoint(pygame.mouse.get_pos())
+    return pygame.mouse.get_pressed()[0] and clear_rect.collidepoint(pygame.mouse.get_pos())
 
 def reset(grid_squares):
-  for square in grid_squares:
-    square.color = "white"
+    for square in grid_squares:
+        square.color = "white"
 
 
 ROWS = 45
@@ -48,9 +48,9 @@ buttons = []
 clear = pygame.image.load('eraser.png')
 
 for i in range(COLS):
-  for j in range(ROWS):
-    square = GridSquare(i*SQUARE_SIZE, j*SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE)
-    grid_squares.append(square)
+    for j in range(ROWS):
+        square = GridSquare(i*SQUARE_SIZE, j*SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE)
+        grid_squares.append(square)
 
 for i, color in enumerate(COLORS):
     button = ColorButton(4*i+20*(i+1), 375, 20, 20, color)
@@ -61,37 +61,37 @@ running = True
 drawing = False
 
 while running:
-  for event in pygame.event.get():
-    if event.type == pygame.QUIT:
-      running = False
-    if event.type == pygame.MOUSEBUTTONDOWN:
-      drawing = True
-    if event.type == pygame.MOUSEBUTTONUP:
-      drawing = False
-    if event.type == pygame.KEYDOWN:
-      if event.key == pygame.K_SPACE:
-        reset(grid_squares)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            drawing = True
+        if event.type == pygame.MOUSEBUTTONUP:
+            drawing = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                reset(grid_squares)
 
-  # Render the drawing grid
-  for square in grid_squares:
-    square.draw(win)
-    if draw_on_grid(drawing, square):
-      square.color = drawing_color
+    # Render the drawing grid
+    for square in grid_squares:
+        square.draw(win)
+        if draw_on_grid(drawing, square):
+            square.color = drawing_color
 
-  # Render the color buttons
-  for button in buttons:
-    button.draw(win)
-    if pick_color(button):
-      drawing_color = button.color
+    # Render the color buttons
+    for button in buttons:
+        button.draw(win)
+        if pick_color(button):
+            drawing_color = button.color
 
-  # Render the clear button
-  clear_rect = clear.get_rect(topleft = (188, 375))
-  win.blit(clear, clear_rect)
-  if pick_clear(clear_rect):
-    drawing_color = "white"
+    # Render the clear button
+    clear_rect = clear.get_rect(topleft = (188, 375))
+    win.blit(clear, clear_rect)
+    if pick_clear(clear_rect):
+        drawing_color = "white"
 
-  clock.tick(60)
-  pygame.display.update()
+    clock.tick(60)
+    pygame.display.update()
 
 
 pygame.quit()
